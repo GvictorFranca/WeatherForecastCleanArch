c98567ca7c3cc15c373a5a72a76cced096fcd678
Gabriel V F Melo <gabrielvfmelo@Gabriels-MacBook-Air.local>
2021-07-31 10:26:07 -0300

    feat: add WhindModel

---
+++ weather_challenge/lib/features/data/models/wind_model.dart
@@ -0,0 +1,19 @@
+import 'package:weather_challenge/features/domain/entities/current_weather/wind_entity.dart';
+
+class WindModel extends WindEntity {
+  final double speed;
+  final int deg;
+
+  WindModel({
+    required this.speed,
+    required this.deg,
+  }) : super(
+          speed: speed,
+          deg: deg,
+        );
+
+  factory WindModel.fromJson(Map<String, dynamic> json) => WindModel(
+        speed: json["speed"].toDouble(),
+        deg: json["deg"],
+      );
+}
