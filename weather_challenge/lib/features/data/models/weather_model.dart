c5d61c9a3cb0067775748bc3005e856c9ccb77fc
Gabriel V F Melo <gabrielvfmelo@Gabriels-MacBook-Air.local>
2021-07-31 10:25:12 -0300

    feat: add WeatherModel

---
+++ weather_challenge/lib/features/data/models/weather_model.dart
@@ -0,0 +1,27 @@
+import 'package:weather_challenge/features/domain/entities/current_weather/weather_entity.dart';
+
+class WeatherModel extends WeatherEntity {
+  final int id;
+  final String main;
+  final String description;
+  final String icon;
+
+  WeatherModel({
+    required this.id,
+    required this.main,
+    required this.description,
+    required this.icon,
+  }) : super(
+          id: id,
+          main: main,
+          description: description,
+          icon: icon,
+        );
+
+  factory WeatherModel.fromJson(Map<String, dynamic> json) => WeatherModel(
+        id: json["id"],
+        main: json["main"],
+        description: json["description"],
+        icon: json["icon"],
+      );
+}
