9b5ee3ae4bd917427992a33c5e1a970d73f077c7
Gabriel V F Melo <gabrielvfmelo@Gabriels-MacBook-Air.local>
2021-07-31 09:36:50 -0300

    feat: add CloudsEntity

---
+++ weather_challenge/lib/features/domain/entities/current_weather/clouds_entity.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class CloudsEntity extends Equatable {
+  final int all;
+
+  List<Object> get props => [];
+
+  CloudsEntity({
+    required this.all,
+  });
+}
