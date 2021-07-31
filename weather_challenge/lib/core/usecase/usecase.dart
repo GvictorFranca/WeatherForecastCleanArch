8af0634628eb7d40cc71780201286328a259c0ae
Gabriel V F Melo <gabrielvfmelo@Gabriels-MacBook-Air.local>
2021-07-31 09:54:15 -0300

    fix: fix UseCase imports

--- weather_challenge/lib/core/usecase/usecase.dart
+++ weather_challenge/lib/core/usecase/usecase.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:weather_challenge/core/error/failures.dart';
+import 'package:weather_challenge/core/error/failures/failures.dart';
 
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
