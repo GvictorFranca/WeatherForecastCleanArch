3c9ec32eadbd1f7defbd2cb0496cef09ef9451bc
Gabriel V F Melo <gabrielvfmelo@Gabriels-MacBook-Air.local>
2021-08-02 11:04:24 -0300

    feat: add HttpErrors exceptions

---
+++ weather_challenge/lib/features/data/http/http_error.dart
@@ -0,0 +1,11 @@
+class BadRequest implements Exception {}
+
+class NotFoundError implements Exception {}
+
+class ServerError implements Exception {}
+
+class ForbiddenError implements Exception {}
+
+class UnauthorizedError implements Exception {}
+
+class InvalidDataError implements Exception {}
