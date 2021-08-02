6c20a43b5611495df32ead4005e50a008290c9a4
Gabriel V F Melo <gabrielvfmelo@Gabriels-MacBook-Air.local>
2021-08-02 11:14:01 -0300

    feat: add QueryParamMapper

---
+++ weather_challenge/lib/features/infra/mapper/query_parameters/query_param_mapper.dart
@@ -0,0 +1,7 @@
+abstract class QueryParamMapper {
+  Map<String, dynamic> mapQueryParam(
+      {required String token,
+      required String cityName,
+      required String units,
+      int? count});
+}
