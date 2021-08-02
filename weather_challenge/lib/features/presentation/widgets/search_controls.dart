27376d3d8418a5f194eeb8abe3cea4befc1e4cdb
Gabriel V F Melo <gabrielvfmelo@Gabriels-MacBook-Air.local>
2021-08-02 11:16:49 -0300

    feat: add SearchControlls

---
+++ weather_challenge/lib/features/presentation/widgets/search_controls.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:validatorless/validatorless.dart';
+import 'package:weather_challenge/features/domain/usecases/get_forecast.dart';
+import 'package:weather_challenge/features/presentation/bloc/forecast/forecast_bloc.dart';
+
+import 'package:weather_challenge/features/presentation/bloc/weather/weather_bloc.dart';
+import 'package:weather_challenge/features/presentation/pages/forecast_page.dart';
+
+import '../../../injection_container.dart';
+
+class SearchControls extends StatefulWidget {
+  const SearchControls({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _SearchControlsState createState() => _SearchControlsState();
+}
+
+class _SearchControlsState extends State<SearchControls> {
+  final _formKey = GlobalKey<FormState>();
+  final _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          key: _formKey,
+          controller: _controller,
+          cursorColor: Colors.black,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a city name',
+          ),
+          validator: Validatorless.required('Please provide a city name!'),
+        ),
+        SizedBox(height: 10),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                  child: Text('Search'),
+                  onPressed: () {
+                    var formValid = _formKey.currentState?.validate() ?? true;
+
+                    if (formValid) {
+                      _controller.clear();
+                      _dispatchCurrentWeather(_controller.text);
+                    }
+                  }),
+            ),
+            SizedBox(width: 10),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: BlocProvider(
+                create: (context) => ForecastBloc(
+                  getForecast: sl<GetForecast>(),
+                ),
+                child: TextButton(
+                    child: Text('Forecast'),
+                    onPressed: () {
+                      var formValid =
+                          _formKey.currentState?.validate() ?? false;
+                      if (!formValid)
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ForecastPage(
+                              cityName: _controller.text,
+                            ),
+                          ),
+                        );
+                    }),
+              ),
+            ),
+            SizedBox(width: 10),
+          ],
+        )
+      ],
+    );
+  }
+
+  void _dispatchCurrentWeather(String inputStr) {
+    _controller.clear();
+    BlocProvider.of<WeatherBloc>(context).add(
+      GetCurrentWeatherEvent(
+        cityName: inputStr,
+      ),
+    );
+  }
+}
